6fde6db6a5bbae65877130a7347b5515c1421600
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-09-15 17:10:44 +0700

    add *ngFor * ngIf structural directive

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -6,11 +6,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent {
+  @Input() name: string;
 
   constructor() {
   }
 
-  showInfo() {
-    alert('Inside Angular Component');
-  }
+  user = {
+    name: 'luan',
+    age: 30,
+  };
+
 }
